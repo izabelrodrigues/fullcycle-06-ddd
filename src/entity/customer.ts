9109f3c240cc37f5f05de759f7d540b9216ee0db
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2022-03-28 20:19:44 -0300

    Adicionando cobertura de testes para customer.ts
    -------------|---------|----------|---------|---------|-------------------------------
    File         | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    -------------|---------|----------|---------|---------|-------------------------------
    All files    |   88.37 |    77.77 |    92.3 |   88.37 |
     address.ts  |   72.22 |    42.85 |      75 |   72.22 | 19-20,22-23,25-26,28-29,33-34
     customer.ts |     100 |      100 |     100 |     100 |
    -------------|---------|----------|---------|---------|-------------------------------
    Test Suites: 1 passed, 1 total
    Tests:       5 passed, 5 total
    Snapshots:   0 total
    Time:        0.448 s, estimated 1 s
    Ran all test suites.

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,11 +1,11 @@
 import Address from "./address";
 
-class Customer {
+export default class Customer {
 
-    _id: string;
-    _name: string;
-    _address!: Address;
-    _active: boolean = false;
+    private _id: string;
+    private _name: string;
+    private _address!: Address;
+    private _active: boolean = false;
 
     constructor(id: string, name: string) {
         this._id = id;
@@ -37,6 +37,14 @@ class Customer {
 
     set address(value: Address) {
         this._address = value;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    isActive(): boolean {
+        return this._active;
     }
 
 }
