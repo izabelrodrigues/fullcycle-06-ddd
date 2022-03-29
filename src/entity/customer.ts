77dc7b86f49db63e3b22976b0f427bcd4cda73db
Izabel Rodrigues <izabel.castro.rodrigues@gmail.com>
2022-03-29 14:05:26 -0300

    Adicionando exemplos de domain service
    -Product and Order
    
    ---------------------|---------|----------|---------|---------|-----------------------------
    File                 | % Stmts | % Branch | % Funcs | % Lines | Uncovered Line #s
    ---------------------|---------|----------|---------|---------|-----------------------------
    All files            |   94.93 |    90.38 |   96.77 |   94.93 |
     entity              |   95.16 |     91.3 |   96.42 |   95.16 |
      address.ts         |   72.22 |    42.85 |      75 |   72.22 | ...,22-23,25-26,28-29,33-34
      customer.ts        |     100 |      100 |     100 |     100 |
      order-item.ts      |     100 |      100 |     100 |     100 |
      order.ts           |     100 |      100 |     100 |     100 |
      product.ts         |     100 |      100 |     100 |     100 |
     service             |   93.33 |    83.33 |     100 |   93.33 |
      order-service.ts   |   89.47 |       75 |     100 |   89.47 | 9-10
      product-service.ts |     100 |      100 |     100 |     100 |
    ---------------------|---------|----------|---------|---------|-----------------------------
    
    Test Suites: 5 passed, 5 total
    Tests:       18 passed, 18 total
    Snapshots:   0 total
    Time:        2.996 s
    Ran all test suites.

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -6,6 +6,7 @@ export default class Customer {
     private _name: string;
     private _address!: Address;
     private _active: boolean = false;
+    private _rewardPoints: number = 0;
 
     constructor(id: string, name: string) {
         this._id = id;
@@ -39,12 +40,24 @@ export default class Customer {
         this._address = value;
     }
 
+    get id(): string {
+        return this._id;
+    }
+
     get name(): string {
         return this._name;
     }
 
     isActive(): boolean {
         return this._active;
+    }
+
+    addRewardPoints(points: number) {
+        this._rewardPoints += points;
+    }
+
+    get rewardPoints(): number {
+        return this._rewardPoints;
     }
 
 }
